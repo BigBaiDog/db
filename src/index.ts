733481e0653a9ffec3b13f7938c214b9864ffca0
yuan <1194423126@qq.com>
2025-05-26 20:38:47 +0800

    env2

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,19 @@ import { drizzle } from "drizzle-orm/mysql2";
 import { createConnection } from "mysql2";
 import { users } from "./schema";
 
-
-export interface Env {
+  type Bindings = {
   HYPERDRIVE: Hyperdrive;
-  }
-const app = new Hono()
+}
+
+const app = new Hono({ Bindings: Bindings })
 
 app.get('/', async (c) => {
   const connection = await createConnection({
-      host: env.HYPERDRIVE.host,
-      user: env.HYPERDRIVE.user,
-      password: env.HYPERDRIVE.password,
-      database: env.HYPERDRIVE.database,
-      port: env.HYPERDRIVE.port,
+      host: c.env.HYPERDRIVE.host,
+      user: c.env.HYPERDRIVE.user,
+      password: c.env.HYPERDRIVE.password,
+      database: c.env.HYPERDRIVE.database,
+      port: c.env.HYPERDRIVE.port,
 
     // Required to enable mysql2 compatibility for Workers
     disableEval: true,
